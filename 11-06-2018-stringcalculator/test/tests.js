966e898332a990e5a93e0997ce064526d79ed1b1
marymafa <marymafa80@gmail.com>
2018-06-11 16:10:50 +0200

    made changes

---
+++ 11-06-2018-stringcalculator/test/tests.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+cd
+describe('Array', function () {
+  it('should return 12 when the value is  present', function () {
+    assert(adding("5,7")).equal(12)
+  });
+});
+
+
